b934f64b2fd87aba7b9186a2e8434dd30f828077
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-04 16:02:17 -0500

    Nav functionality with 2 total event listeners used added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+
+//nav hover color change
+function navHover(event){
+    event.target.style.color = 'blue';
+}
+function navLeave(event){
+    event.target.style.color = 'black';
+}
+
+const navH = document.querySelector('nav :nth-child(1)');
+const navA = document.querySelector('nav :nth-child(2)');
+const navB = document.querySelector('nav :nth-child(3)');
+const navC = document.querySelector('nav :nth-child(4)');
+
+navH.addEventListener('mouseenter', navHover);
+navA.addEventListener('mouseenter', navHover);
+navB.addEventListener('mouseenter', navHover);
+navC.addEventListener('mouseenter', navHover);
+navH.addEventListener('mouseleave', navLeave);
+navA.addEventListener('mouseleave', navLeave);
+navB.addEventListener('mouseleave', navLeave);
+navC.addEventListener('mouseleave', navLeave);
+
+
+// const test = Array.from(document.querySelector('nav'));
+// console.log(test);
